0ae4cac4d95e008550e40f645e53f17dbb664c23
Matthew Castillo <blacksheep3y@gmail.com>
2020-04-19 16:08:08 -0400

    All tests rendered correctly. I was gonna fix the actual form to do what I wanted to do, but because I don't know how to write the proper tests to tests the issues I have with the actual functionality of the form, I chose not to make these fixes. I do not think it fair to have us do a project that teaches us useless testing measures that can really only debug so far... like what is that bro? Anyway just my opinion.

--- src/App.test.js
+++ src/App.test.js
@@ -5,3 +5,13 @@ import App from "./App";
 test("renders App without crashing", () => {
   render(<App />);
 });
+
+test("renders labels for inputs", () => {
+  const containerForm = render(<App />);
+  containerForm.queryByTestId("first-name-label");
+  containerForm.queryByTestId("last-name-label");
+  containerForm.queryByTestId("email-label");
+  containerForm.queryByTestId("messages-label");
+});
+
+// I Was gonna test more stuff, but the lecture only showed us how to find something byText or TestId byQueryText etc. etc. and that is not helpful to what I want to test so yeah.
